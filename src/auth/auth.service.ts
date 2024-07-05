fbdfcfa78a086c10842549a3638878ec7972bd7a
Nguyễn Đức Anh <ducanhnguyend21cntt@gmail.com>
2024-07-05 09:49:20 +0700

    Update auth.service.ts

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,7 +8,6 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../user/entities/user.entity';
-import { Response } from 'express';
 import { Role } from '../role/entities/role.entity';
 import { UserLoginDto } from './dto/auth-login.dto';
 import { RoleService } from '../role/role.service';
@@ -27,31 +26,36 @@ export class AuthService {
   ) {}
 
   async login(userLoginDto: UserLoginDto) {
-    const user = await this.userRepository.findOne({
-      where: { email: userLoginDto.email },
-    });
-    if (!user) {
-      throw new UnauthorizedException('Invalid email');
+    try {
+      const user = await this.userRepository.findOne({
+        where: { email: userLoginDto.email },
+      });
+      if (!user) {
+        throw new UnauthorizedException('Invalid email');
+      }
+      if (
+        this.userService.isValidPassword(
+          user.password,
+          this.userService.getHashPassword(userLoginDto.password),
+        )
+      ) {
+        throw new UnauthorizedException('Invalid password');
+      }
+      const roles = await this.roleService.findRoleByUser(user.id);
+      
+      const payload = {
+        sub: 'from server',
+        iss: 'token login',
+        id: user.id,
+        email: user.email,
+        roles: roles['roleEnum'],
+      };
+      
+      return {
+        access_token: await this.jwtService.signAsync(payload),
+      };
+    } catch (error) {
+      return 'Failed!';
     }
-    if (
-      this.userService.isValidPassword(
-        user.password,
-        this.userService.getHashPassword(userLoginDto.password),
-      )
-    ) {
-      throw new UnauthorizedException('Invalid password');
-    }
-    const roles = await this.roleService.findRoleByUser(user.id);
-    
-    const payload = {
-      sub: 'from server',
-      iss: 'token login',
-      id: user.id,
-      email: user.email,
-      roles: roles['roleEnum'],
-    };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
   }
 }
